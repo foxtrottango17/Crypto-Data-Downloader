f6ba5ddf57010c8debe5dee1635bbc641c066035
Farrel <83548215+foxtrottango17@users.noreply.github.com>
2022-03-01 13:25:31 +0700

    Add files via upload

--- DownloadData.js
+++ DownloadData.js
@@ -14,7 +14,7 @@ let datas = [];
 
 async function historicalData(){
 
-  if (endDate > new Date().getTime()) {
+  if(endDate > new Date().getTime()){
     console.log("endDate is geater than current time. This will cause an inifinate loop. Please change the endDate.")
     return;
   }
@@ -28,7 +28,7 @@ async function historicalData(){
 
   try{
 
-    for (let i = 1; i < data.length; i++){
+    for(let i = 1; i < data.length; i++){
       datas.push({
         'openTime': data[i].openTime,
         'open': data[i].open,
@@ -41,46 +41,46 @@ async function historicalData(){
 
     startDate = (datas[datas.length - 1].openTime); 
 
-    if (startDate != endDate){
+    if(startDate != endDate){
       historicalData();
 
-    } else if (startDate === endDate){
+    }else if (startDate === endDate){
 
       const tempStartInfo = new Date(datas[1].openTime);
       const tempEndInfo = new Date(datas[datas.length - 1].openTime);
       const startInfo = tempStartInfo.getFullYear() + '/' + (tempStartInfo.getMonth() + 1) + '/' + tempStartInfo.getDate();
       const endInfo = tempEndInfo.getFullYear() + '/' + (tempEndInfo.getMonth() + 1) + '/' + tempEndInfo.getDate();
 
+      const csvWriter = createCsvWriter({
+        path: './backtester/BTCUSDT.csv',
+        header: [
+          {id: 'openTime', title: 'Open Time'},
+          {id: 'open', title: 'Open'},
+          {id: 'high', title: 'High'},
+          {id: 'low', title: 'Low'},
+          {id: 'close', title: 'Close'},
+          {id: 'volume', title: 'Volume'}
+        ]
+      });  
+
       if(saveAs === 'JSON'){
 
         fs.writeFile('./backtester/BTCUSDT.json', JSON.stringify(datas), err =>{
-          if(err){
+          if (err){
             console.log(err);
-          }else{
+          } else {
             console.log("Successfully Downloaded Data AS JSON From " + startInfo + ' To: ' + endInfo);
           }
         }); 
 
-      } else if (saveAs === 'CSV'){
-
-        const csvWriter = createCsvWriter({
-          path: './backtester/BTCUSDT.csv',
-          header: [
-            {id: 'openTime', title: 'Open Time'},
-            {id: 'open', title: 'Open'},
-            {id: 'high', title: 'High'},
-            {id: 'low', title: 'Low'},
-            {id: 'close', title: 'Close'},
-            {id: 'volume', title: 'Volume'}
-          ]
-        });  
+      }else if (saveAs === 'CSV'){
 
         csvWriter.writeRecords(datas)  
         .then(() => {
             console.log("Successfully Downloaded Data As CSV From " + startInfo + ' To: ' + endInfo);
         });
 
-      } else {
+      }else{
         fs.writeFile('./backtester/BTCUSDT.json', JSON.stringify(datas), err =>{
           if(err){
             console.log(err);
@@ -89,22 +89,11 @@ async function historicalData(){
           }
         });
 
-        const csvWriter = createCsvWriter({
-          path: './backtester/BTCUSDT.csv',
-          header: [
-            {id: 'openTime', title: 'Open Time'},
-            {id: 'open', title: 'Open'},
-            {id: 'high', title: 'High'},
-            {id: 'low', title: 'Low'},
-            {id: 'close', title: 'Close'},
-            {id: 'volume', title: 'Volume'}
-          ]
-        });  
-
         csvWriter.writeRecords(datas)  
         .then(() => {
             console.log("Successfully Downloaded Data As CSV From " + startInfo + ' To: ' + endInfo);
         });
+
       }
     }
   }catch(error){
